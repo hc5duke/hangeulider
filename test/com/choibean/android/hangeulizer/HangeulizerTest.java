192ca35bafdb930b3888036a7ec9c3504f8b340e
hc5duke <hc5duke@gmail.com>
2010-02-13 00:03:33 -0800

    old test directory doesn't do anything

--- test/com/choibean/android/hangeulizer/HangeulizerTest.java
+++
@@ -1,9 +0,0 @@
-package com.choibean.android.hangeulizer;
-
-import android.test.AndroidTestCase;
-
-public class HangeulizerTest extends AndroidTestCase {
-	public void testSomething() {
-		assertEquals(1, 1);
-	}
-}
